feb9cca7c8c6742e0fb946aa5fed8bd398dce77d
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-05-11 13:24:12 -0500

    added backend vercel compatibility

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="header">
       <nav className="navbar navbar-expand-md">
-        <a className="navbar-brand" href="#">Keith Gaines</a>
+        <a className="navbar-brand" href="/">Keith Gaines</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon">
             <span className="navbar-toggler-icon">
